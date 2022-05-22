8a48fb2dea2181f7ce17c9a50fb5428b2bdac5e2
shushant kumar <104075647+everythingcanbedone@users.noreply.github.com>
2022-05-22 14:18:26 +0530

    Create Day of the Programmer.py

---
+++ data structure and algorithms/implementation/Day of the Programmer.py
@@ -0,0 +1,8 @@
+year = int(input())
+if ((year < 1918) and (year % 4 == 0)) or \
+    ((year > 1918) and ((year % 400 == 0) or (year % 4 == 0 and year % 100 != 0))):
+    print('12.09.' + str(year))
+elif (year == 1918):
+    print('26.09.' + str(year))
+else:
+    print('13.09.' + str(year))
