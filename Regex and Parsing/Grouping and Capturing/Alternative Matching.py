5691344e8b041996a90395057f8043f20d3ec999
shushant kumar <104075647+everythingcanbedone@users.noreply.github.com>
2022-06-21 20:44:20 +0530

    Update Alternative Matching.py

--- Regex and Parsing/Grouping and Capturing/Alternative Matching.py
+++ Regex and Parsing/Grouping and Capturing/Alternative Matching.py
@@ -1,3 +1,3 @@
-# import note . will match any character so use escape \. to match a dot 
+# important note . will match any character so use escape \. to match a dot 
 
 ^(Mr\.|Mrs\.|Ms\.|Dr\.|Er\.)[a-zA-Z]+$
