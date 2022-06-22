0eecb792e07033c00f992572bc73755369e48253
shushant kumar <104075647+everythingcanbedone@users.noreply.github.com>
2022-06-22 19:36:56 +0530

    Create Detect HTML Tags.py

---
+++ Regex and Parsing/Applications/Detect HTML Tags.py
@@ -0,0 +1,17 @@
+#    \s for take care of space
+#take care of opening tag not look into the closing tag
+
+
+Regex = r'<\s*(\w+)'
+import re
+
+list = set()
+
+for _ in range(int(input())):
+    _t = input()
+    found = re.findall(Regex, _t,re.DOTALL)
+    for tag in found:
+        if tag not in list:
+            list.add(tag)
+
+print(';'.join(sorted(list)))
