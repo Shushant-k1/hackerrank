b80988198f770766b9e793d7626e728367451209
shushant kumar <104075647+everythingcanbedone@users.noreply.github.com>
2022-06-24 14:00:44 +0530

    Update Saying Hi.py

--- Regex and Parsing/Applications/Saying Hi.py
+++ Regex and Parsing/Applications/Saying Hi.py
@@ -1,7 +1,9 @@
 # Enter your code here. Read input from STDIN. Print output to STDOUT
+
+#r"[hH][Ii]\s[^dD]"
 import re
 
-pattern=r"^(h|H)(I|i)\s[^d]"
+pattern=r"^(h|H)(I|i)\s[^dD]"
 for i in range(int(input())):
     s=input()
     if re.match(pattern,s):
