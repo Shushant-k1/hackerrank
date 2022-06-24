d564f1c70b18687c68c04c134bc80b1d560455c9
shushant kumar <104075647+everythingcanbedone@users.noreply.github.com>
2022-06-24 14:22:51 +0530

    Create HackerRank Language.py

---
+++ Regex and Parsing/Applications/HackerRank Language.py
@@ -0,0 +1,9 @@
+import re
+
+pattern = r"^[1-9]\d{4}\s(C|CPP|JAVA|PYTHON|PERL|PHP|RUBY|CSHARP|HASKELL|CLOJURE|BASH|SCALA|ERLANG|CLISP|LUA|BRAINFUCK|JAVASCRIPT|GO|D|OCAML|R|PASCAL|SBCL|DART|GROOVY|OBJECTIVEC)$"
+
+for _ in range(int(input())):
+    if re.search(pattern,input()):
+        print("VALID")
+    else:
+        print("INVALID")
