846f225c6110de2f07784e18773ce7b4ec0cce89
shushant kumar <104075647+everythingcanbedone@users.noreply.github.com>
2022-06-24 14:23:40 +0530

    Update HackerRank Language.py

--- Regex and Parsing/Applications/HackerRank Language.py
+++ Regex and Parsing/Applications/HackerRank Language.py
@@ -1,3 +1,5 @@
+# firts sting must contain only digit then followed by a space
+
 import re
 
 pattern = r"^[1-9]\d{4}\s(C|CPP|JAVA|PYTHON|PERL|PHP|RUBY|CSHARP|HASKELL|CLOJURE|BASH|SCALA|ERLANG|CLISP|LUA|BRAINFUCK|JAVASCRIPT|GO|D|OCAML|R|PASCAL|SBCL|DART|GROOVY|OBJECTIVEC)$"
