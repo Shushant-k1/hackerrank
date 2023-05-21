429d952d7f12033d9bc99f866c87a56e800c0d2e
shushant kumar <rekhaubgb@gmail.com>
2023-05-21 18:23:44 +0530

    Create Two_Characters.py

---
+++ data structure and algorithms/strings/Two_Characters.py
@@ -0,0 +1,18 @@
+def alternate(s):
+    comb = combinations(set(s), 2)
+    max_one = 0
+    for a, b in comb:
+        stack = []
+        check = True
+        for i in s:
+            if i in [a,b]:
+                if not stack:
+                    stack.append(i)
+                elif(i == stack[-1]):
+                    check = False
+                    break
+                else:
+                    stack.append(i)
+        if check and max_one < len(stack):
+            max_one = len(stack)
+    return max_one
